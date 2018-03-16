5624f5af4afba9a91e0ee00962a86dd056c59347
Andrew Tu <drewtu2@yahoo.com>
2018-03-16 10:37:35 -0400

    updated move factory to produce proper moves

--- animator/src/cs3500/animator/view/ViewFactory.java
+++ animator/src/cs3500/animator/view/ViewFactory.java
@@ -1,5 +1,8 @@
 package cs3500.animator.view;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Factory class that holds methods to create views on demand.
  */
@@ -9,15 +12,29 @@ public class ViewFactory {
    * Factory method that creates a specific instance of a view dependent on given input.
    * @param name String representing the type of view required
    * @return IView instance
+   * @throws IOException
    */
-  public static IView getAppearAnimation(String name) {
-    switch(name) {
-      case "text":
-        return new TextView();
+  public static IView getView(String name, String outputFile) throws IOException {
+    Appendable myAppendable;
+
+    // Handle null case
+    if (outputFile == null) {
+      throw new NullPointerException();
+    }
+    // Handle not out case
+    if (outputFile != "out") {
+      myAppendable = new FileWriter(outputFile, true); //true tells to append data.
+    } else { // Handle out case
+      myAppendable = System.out;
+    }
+
+    switch(name.toLowerCase()) {
       case "visual":
         return new VisualView();
+      case "text":
+        return new TextView(myAppendable);
       case "svg":
-        return new SVGView();
+        return new SVGView(myAppendable);
         default:
           throw new IllegalArgumentException("invalid view type");
     }
