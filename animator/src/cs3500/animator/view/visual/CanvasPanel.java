a17df24c879a9eaa30a9da24e480f5f5bf700d1d
Andrew Tu <drewtu2@yahoo.com>
2018-03-20 00:13:22 -0400

    added visual view

---
+++ animator/src/cs3500/animator/view/visual/CanvasPanel.java
@@ -0,0 +1,81 @@
+package cs3500.animator.view.visual;
+
+import static cs3500.animator.util.MyUtil.checkNull;
+
+import cs3500.animator.model.IModelView;
+import cs3500.animator.shape.IAnimatedShape;
+import cs3500.animator.shape.IShape;
+import cs3500.animator.shape.RGBColor;
+import cs3500.animator.shape.dimension.WidthHeightDim;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.Map;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+public class CanvasPanel extends JPanel{
+    private IModelView myMv;
+    private Map<String, IAnimatedShape> state;
+    private int tickNum;
+
+    public CanvasPanel()  {
+      tickNum = 0;
+      setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
+
+    public CanvasPanel(BorderLayout bl) {
+      this.setLayout(bl);
+    }
+
+    public Dimension getPreferredSize() {
+      return new Dimension(500, 500);
+    }
+
+    public void setModelView(IModelView mv) {
+      checkNull(mv);
+      myMv = mv;
+      state = myMv.getFullState();
+      checkNull(state);
+    }
+
+    public void setTickNumber(int number) {
+      tickNum = number;
+      System.out.println("Tick num: " + Integer.toString(tickNum));
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      RGBColor shapeColor;
+      IShape shapeState;
+
+      if(state != null) {
+        for (IAnimatedShape shape : state.values()) {
+          shapeColor = shape.getColor();
+          shapeState = shape.stateAt(tickNum);
+
+          g.setColor(new Color(shapeColor.getRed(), shapeColor.getGreen(), shapeColor.getBlue()));
+          if(shape.getOpacity() == 1) {
+            switch (shapeState.getType()) {
+              case RECTANGLE:
+                g.drawRect((int) shapeState.getPosition().getX(),
+                    (int) shapeState.getPosition().getY(),
+                    (int) ((WidthHeightDim) shapeState.getDimension()).getWidth(),
+                    (int) ((WidthHeightDim) shapeState.getDimension()).getHeight());
+                break;
+              case OVAL:
+                g.drawOval((int) shapeState.getPosition().getX(),
+                    (int) shapeState.getPosition().getY(),
+                    (int) ((WidthHeightDim) shapeState.getDimension()).getWidth(),
+                    (int) ((WidthHeightDim) shapeState.getDimension()).getHeight());
+                break;
+              default:
+                System.err.println("Unexpected Shape Type...");
+            }
+          }
+        }
+      }
+    }
+}
---
+++ animator/src/cs3500/animator/view/visual/VisualView.java
@@ -0,0 +1,86 @@
+package cs3500.animator.view.visual;
+
+import static cs3500.animator.util.MyUtil.checkNull;
+
+import cs3500.animator.model.IModelView;
+import cs3500.animator.view.IView;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JScrollBar;
+import javax.swing.Timer;
+
+/**
+ * Class representing the visual view of an animator.
+ */
+public class VisualView extends JFrame implements IView {
+
+  private int frameWidth;
+  private int frameHeight;
+  private int speed; // tics/second
+
+  private final CanvasPanel canvas;
+
+  private JScrollBar hbar;
+  private JScrollBar vbar;
+  private Timer timer;
+
+  private int secondsCount;
+
+  public VisualView() {
+    super();
+
+    frameHeight = 500;
+    frameWidth = 500;
+
+    this.setTitle("Simple Animator");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(frameWidth, frameHeight);
+
+    //canvas = new JPanel(new BorderLayout());
+    canvas = new CanvasPanel(new BorderLayout());
+
+    hbar = new JScrollBar(JScrollBar.HORIZONTAL);
+    vbar = new JScrollBar(JScrollBar.VERTICAL);
+
+    canvas.add(hbar, BorderLayout.SOUTH);
+    canvas.add(vbar, BorderLayout.EAST);
+
+    this.setContentPane(canvas);
+
+    //myFrame.pack();
+    this.setVisible(true);
+
+    secondsCount = 0;
+    timer = new Timer(1000,
+        (ActionEvent e) -> {
+          System.out.println("Timer Event fired");
+          canvas.setTickNumber(secondsCount * speed);
+          secondsCount++;
+          repaint();
+        });
+
+  }
+
+  @Override
+  public void show(IModelView state, int tempo) throws IOException {
+    System.out.println("Entered show...");
+    checkNull(state);
+    speed = tempo;
+    canvas.setModelView(state);
+    System.out.println("Set Model...");
+    timer.start();
+    System.out.println("Started Timer...");
+  }
+
+  /**
+   * Returns the time for a corresponding tick.
+   * @param tick the tick.
+   * @return the time
+   */
+  private float tick2Time(float tick) {
+    return tick * speed;
+  }
+}
+
