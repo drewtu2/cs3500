3c816a294d85afe277437475bb0ab81e7f1262f9
NikithaKoppu <35382140+NikithaKoppu@users.noreply.github.com>
2018-03-20 19:04:13 -0400

    controller testing

--- animator/test/ControllerTest.java
+++ animator/test/ControllerTest.java
@@ -4,6 +4,12 @@ import static junit.framework.TestCase.fail;
 import cs3500.animator.controller.AnimatorController;
 import cs3500.animator.controller.AnimatorController.Builder;
 import cs3500.animator.controller.IController;
+import cs3500.animator.model.AnimatorModel;
+import cs3500.animator.model.IAnimatorModel;
+import cs3500.animator.util.AnimationFileReader;
+import cs3500.animator.view.IView;
+import cs3500.animator.view.ViewFactory;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.junit.Before;
@@ -29,57 +35,97 @@ public class ControllerTest {
     goodArgs = new String[]{"-if", "assignment6_starter/toh-3.txt", "-iv", "visual", "-speed", "20", "-o", "out.txt"};
     badInputFile = new String[]{"-if", "test.tx", "-iv", "visual", "-speed", "20", "-o", "out.txt"};
     noInputFile = new String[]{"-iv", "visual", "-speed", "20", "-o", "out.txt"};
-    noView = new String[]{"-if", "test.tx", "-speed", "20", "-o", "out.txt"};
-    badFlag = new String[]{"-id", "test.tx", "-iv", "visual", "-speed", "20", "-o", "out.txt"};
-    badView = new String[]{"-if", "test.tx", "-iv", "vis", "-speed", "20", "-o", "out.txt"};
-    badSpeed = new String[]{"-if", "test.tx", "-iv", "vis", "-speed", "-20", "-o", "out.txt"};
-    missingArgument = new String[]{"-if", "test.tx", "-iv", "vis", "-speed", "-20", "-o"};
+    noView = new String[]{"-if", "assignment6_starter/toh-3.txt", "-speed", "20", "-o", "out.txt"};
+    badFlag = new String[]{"-id", "assignment6_starter/toh-3.txt", "-iv", "visual", "-speed", "20", "-o", "out.txt"};
+    badView = new String[]{"-if", "assignment6_starter/toh-8.txt", "-iv", "vis", "-speed", "20",
+            "-o", "out.txt"};
+    badSpeed = new String[]{"-if", "assignment6_starter/toh-8.txt", "-iv", "visual", "-speed",
+            "-20", "-o", "out.txt"};
+    missingArgument = new String[]{"-if", "assignment6_starter/toh-3.txt", "-iv", "visual", "-speed", "20", "-o"};
 }
 
   @Test
   public void testBuilder() {
 
     IController myController;
+    IController testValController;
     try {
+      AnimationFileReader mr = new AnimationFileReader();
+      AnimatorModel.Builder mb = new AnimatorModel.Builder();
+      IAnimatorModel myModel = mr.readFile("assignment6_starter/toh-3.txt", mb);
+      IView myView = ViewFactory.getView("visual", "out.txt");
+      int speed = 20;
+      testValController = new AnimatorController(myModel, myView, speed);
       myController = builder.buildFromInputArgs(goodArgs);
-      assertEquals(null, myController);
-    } catch (IOException e) {
-      // meh..
-      fail("controller not equal");
+      assertEquals(testValController, myController);
+    }
+    catch(IOException e) {
+      // someting
+      //fail("controller not equal");
     }
   }
 
-  @Test(expected = FileNotFoundException.class)
-  public void testInputFileNotExist() {
+  @Test(expected = IOException.class)
+  public void testInputFileNotExist() throws IOException {
+      builder.buildFromInputArgs(badInputFile);
+
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNoInputFileParam() {
 
     try {
-      builder.buildFromInputArgs(badInputFile);
+      builder.buildFromInputArgs(noInputFile);
+      fail("no input file detected");
     } catch (IOException e) {
-      // meh..
-      fail("controller not equal");
+      //fail("no input file detected");
     }
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testNoInputFile() {
+  public void testNoView() throws IOException{
 
-    try {
-      builder.buildFromInputArgs(badInputFile);
-    } catch (IOException e) {
-      // meh..
-      fail("controller not equal");
-    }
-  }
-
-  @Test(expected = IllegalArgumentException.class)
-  public void testNoView() {
-
-    try {
       builder.buildFromInputArgs(noView);
+
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testBadFlag() {
+
+    try {
+      builder.buildFromInputArgs(badFlag);
     } catch (IOException e) {
-      // meh..
-      fail("controller not equal");
+      fail("bad flag detected. unable to read parameters");
     }
   }
 
+  @Test(expected = IllegalArgumentException.class)
+  public void testBadView() {
+
+    try {
+      builder.buildFromInputArgs(badView);
+    } catch (IOException e) {
+      fail("bad view detected. unable to read parameters");
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testBadSpeed() {
+
+    try {
+      builder.buildFromInputArgs(badSpeed);
+    } catch (IOException e) {
+      fail("bad speed detected. unable to read parameters");
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMissingArgument() {
+
+    try {
+      builder.buildFromInputArgs(missingArgument);
+    } catch (IOException e) {
+      fail("missing argument from input");
+    }
+  }
 }
