6eaf66189c97d0b942b84c6f6905c76d807ca98b
NikithaKoppu <35382140+NikithaKoppu@users.noreply.github.com>
2018-03-21 16:38:26 -0400

    adjusted dur values in expected output

--- animator/test/SVGViewTest.java
+++ animator/test/SVGViewTest.java
@@ -84,16 +84,16 @@ public class SVGViewTest {
             "\txmlns=\"http://www.w3.org/2000/svg\">\n" +
             "\n" +
             "<rect id=\"R\" x=\"200\" y=\"200\" width=\"50\" height=\"100\" fill=\"rgb(255,0,0)\" visibility=\"visible\" >\n" +
-            "\t<animate attributeType=\"xml\" begin=\"1.0s\" dur=\"4.0s\" attributeName=\"x\" from=\"200\" to=\"300\" fill=\"freeze\" />\n" +
-            "\t<animate attributeType=\"xml\" begin=\"4.0s\" dur=\"10.0s\" attributeName=\"width\" from=\"50\" to=\"20\" fill=\"freeze\" />\n" +
-            "\t<animate attributeType=\"xml\" begin=\"4.0s\" dur=\"10.0s\" attributeName=\"height\" from=\"100\" to=\"20\" fill=\"freeze\" />\n" +
-            "\t<animate attributeType=\"xml\" begin=\"2.0s\" dur=\"5.0s\" attributeName=\"fill\" from=\"Color: (1.0, 0.0, 0.0)\" to=\"Color: (0.0, 1.0, 0.0)\" fill=\"freeze\" />\n" +
+            "\t<animate attributeType=\"xml\" begin=\"1.0s\" dur=\"3.0s\" attributeName=\"x\" from=\"200\" to=\"300\" fill=\"freeze\" />\n" +
+            "\t<animate attributeType=\"xml\" begin=\"4.0s\" dur=\"6.0s\" attributeName=\"width\" from=\"50\" to=\"20\" fill=\"freeze\" />\n" +
+            "\t<animate attributeType=\"xml\" begin=\"4.0s\" dur=\"6.0s\" attributeName=\"height\" from=\"100\" to=\"20\" fill=\"freeze\" />\n" +
+            "\t<animate attributeType=\"xml\" begin=\"2.0s\" dur=\"3.0s\" attributeName=\"fill\" from=\"Color: (1.0, 0.0, 0.0)\" to=\"Color: (0.0, 1.0, 0.0)\" fill=\"freeze\" />\n" +
             "</rect>\n" +
             "\n" +
             "<ellipse id=\"C\" cx=\"500\" cy=\"100\" rx=\"60\" ry=\"30\" fill=\"rgb(0,0,255)\" visibility=\"visible\" >\n" +
             "\t<animate attributeType=\"xml\" begin=\"0.0s\" dur=\"0.0s\" attributeName=\"visibility\" fill=\"freeze\" />\n" +
-            "\t<animate attributeType=\"xml\" begin=\"2.0s\" dur=\"5.0s\" attributeName=\"cx\" from=\"500\" to=\"600\" fill=\"freeze\" />\n" +
-            "\t<animate attributeType=\"xml\" begin=\"2.0s\" dur=\"5.0s\" attributeName=\"cy\" from=\"100\" to=\"400\" fill=\"freeze\" />\n" +
+            "\t<animate attributeType=\"xml\" begin=\"2.0s\" dur=\"3.0s\" attributeName=\"cx\" from=\"500\" to=\"600\" fill=\"freeze\" />\n" +
+            "\t<animate attributeType=\"xml\" begin=\"2.0s\" dur=\"3.0s\" attributeName=\"cy\" from=\"100\" to=\"400\" fill=\"freeze\" />\n" +
             "</ellipse>\n" +
             "\n" +
             "</svg>", testString.toString());
